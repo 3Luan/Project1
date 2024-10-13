dfa00f79c68e762aa66d87860894e500e6cb7e34
3Luan <nguyenthanhluanbntsbd@gmail.com>
2024-10-13 17:22:17 +0700

    config mongoDB, openAPI, app

---
+++ Backend/src/main/java/com/project1/configuration/MongoConfig.java
@@ -0,0 +1,9 @@
+package com.project1.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
